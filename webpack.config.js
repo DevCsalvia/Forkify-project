fd500e9e130fe11c0968ab82998cfa3aa9e4de16
unknown <vip_heromen_vip@mail.ru>
2019-12-21 02:42:44 +0500

    Webpack and Babel initialization

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 //Here we get the absolute path to the file
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-	entry: './src/js/index.js',
+	entry: ['./src/js/index.js'],
 	output: {
 		//__dirname current absolute path
-		path: path.resolve(__dirname, 'dist/js'),
-		filename: 'bundle.js'
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
 	},
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
